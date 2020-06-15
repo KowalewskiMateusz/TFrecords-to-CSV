d26d4db706f0969a70e3b7fb6b2a6f2a8afbb7b5
KowalewskiMateusz <39586192+KowalewskiMateusz@users.noreply.github.com>
2020-06-15 12:31:10 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+import tensorflow as tf
+import pandas as pd
+import youtube_dl
+import os
+from pydub import AudioSegment
+
+"""
+path_features = TFrecords directory
+path_videos = directory in which you want to save audio from youtube
+
+CSV file is created in the script directory
+
+"""
+path_features = "C:\\Users\\kowal\\Desktop\\Nowy folder\\audioset_v1_embeddings\\eval"
+path_videos = "C:\\Users\\kowal\\Desktop\\Pliki"
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'wav',
+        'preferredquality': '192',
+    }],
+    'outtmpl': path_videos + '/%(title)s.%(ext)s',
+}
+dict = {}
+i = 0
+
+if __name__ == '__main__':
+    filenames = [os.path.join(path_features,file) for file in os.listdir(path_features)]
+    raw_dataset = tf.data.TFRecordDataset(filenames)
+    for raw_record in raw_dataset:
+        example = tf.train.SequenceExample()
+        example.ParseFromString(raw_record.numpy())
+
+        try:
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                info_dict = ydl.extract_info(os.path.join('http://www.youtube.com/watch?v=' +
+                                           str(example.context.feature["video_id"].bytes_list.value[0].decode())), download=True)
+
+                video_title = info_dict.get('title', None)
+                audio = AudioSegment.from_file(os.path.join(path_videos, video_title + ".wav"))
+                t1 = example.context.feature["start_time_seconds"].float_list.value[0] * 1000
+                t2 = example.context.feature["end_time_seconds"].float_list.value[0] * 1000
+                audio = audio[t1:t2]
+                audio.export(os.path.join(path_videos, video_title + ".wav"), format="wav")
+
+        except:
+            continue
+
+        dict[i]  = {"video_id" : example.context.feature["video_id"].bytes_list.value[0].decode(),
+                   "start_time_seconds" : example.context.feature["start_time_seconds"].float_list.value[0],
+                   "end_time_seconds" : example.context.feature["end_time_seconds"].float_list.value[0],
+                   "path" : os.path.join(path_videos, video_title + ".wav")}
+        i += 1
+
+    df = pd.DataFrame.from_dict(dict, "index")
+    df.to_csv(r'TFrecord_to_csv.csv', index = False)
+
